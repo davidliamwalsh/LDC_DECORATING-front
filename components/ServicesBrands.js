9558cf29c7419d7046e470cbf2acf3460aca920d
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-03-16 11:59:15 +0000

    data-aos added

--- components/ServicesBrands.js
+++ components/ServicesBrands.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 class ServicesBrands extends Component {
   render () {
     return <div className="c-services-brands">
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.johnstonespaint.com/'>
             <a><img src="/static/johnstone.jpg" alt=""/></a>
@@ -14,7 +14,7 @@ class ServicesBrands extends Component {
           <p>Johnstones offer unrivalled quality and reliability to help to transform the homes of people across the country.</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.crownpaints.co.uk/'>
             <a><img src="/static/crown.jpg" alt=""/></a>
@@ -24,7 +24,7 @@ class ServicesBrands extends Component {
           <p>Crown Paints are a world leading coatings supplier to the decorative, protective, marine, container and yacht markets.</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.dulux.co.uk/'>
             <a><img src="/static/dulux.jpg" alt=""/></a>
@@ -34,7 +34,7 @@ class ServicesBrands extends Component {
           <p>Johnstones products have been at the forefront of the UK paint market since 1890. They offer unrivalled quality</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.farrow-ball.com/'>
             <a><img src="/static/farrowball.jpg" alt=""/></a>
@@ -44,7 +44,7 @@ class ServicesBrands extends Component {
           <p>Johnstones products have been at the forefront of the UK paint market since 1890. They offer unrivalled quality</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.glidden.com/'>
             <a><img src="/static/glidden.jpg" alt=""/></a>
@@ -54,7 +54,7 @@ class ServicesBrands extends Component {
           <p>Johnstones products have been at the forefront of the UK paint market since 1890. They offer unrivalled quality</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//leylandtrade.com/'>
             <a><img src="/static/leyland.jpg" alt=""/></a>
@@ -64,7 +64,7 @@ class ServicesBrands extends Component {
           <p>We've built up a strong reputation by delivering top quality paint for over 90 years..</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.macphersontrade.co.uk/'>
             <a><img src="/static/macpherson.jpg" alt=""/></a>
@@ -74,7 +74,7 @@ class ServicesBrands extends Component {
           <p>Johnstones products have been at the forefront of the UK paint market since 1890. They offer unrivalled quality</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.polycell.co.uk/'>
             <a><img src="/static/polycell.jpg" alt=""/></a>
@@ -84,7 +84,7 @@ class ServicesBrands extends Component {
           <p>Johnstones products have been at the forefront of the UK paint market since 1890. They offer unrivalled quality</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.sikkens.com/'>
             <a><img src="/static/sikkens.jpg" alt=""/></a>
@@ -94,7 +94,7 @@ class ServicesBrands extends Component {
           <p>Johnstones products have been at the forefront of the UK paint market since 1890. They offer unrivalled quality</p>
         </div>
       </div>
-      <div className="c-services-brands__item">
+      <div className="c-services-brands__item" data-aos="fade">
         <div className="c-services-brands__item--image">
           <Link href='//www.stylelibrary.com/zoffany/'>
             <a><img src="/static/zoffany.jpg" alt=""/></a>
