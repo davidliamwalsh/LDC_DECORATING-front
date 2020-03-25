6f9e0fd700472e7d0a2c1c9a67fa41e131b0052d
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-03-25 00:00:51 +0000

    banner images

--- components/SectorsGrid.js
+++ components/SectorsGrid.js
@@ -56,32 +56,6 @@ class SectorsGrid extends Component {
             <h4>OFFICE REDECORATION SERVICES</h4>
           </div>
         </div>
-
-        <div className="c-service-sector--grid__item" data-aos="fade-right">
-          <div className="c-service-sector--grid__item--title">
-            <h2>Decorative Wall Coverings</h2>
-            <hr className="o-line" />
-          </div>
-          <Link href='/sectors/'>
-            <a><div className="c-service-sector--grid__item--image c-service-sector--grid__item--wall-coverings" /></a>
-          </Link>
-          <div className="c-service-sector--grid__item--footer">
-            <h4>Visual Wall Decorations</h4>
-          </div>
-        </div>
-
-        <div className="c-service-sector--grid__item" data-aos="fade-left">
-          <div className="c-service-sector--grid__item--title">
-            <h2>Decorative Wall Coverings</h2>
-            <hr className="o-line" />
-          </div>
-          <Link href='/sectors/'>
-            <a><div className="c-service-sector--grid__item--image c-service-sector--grid__item--wall-coverings" /></a>
-          </Link>
-          <div className="c-service-sector--grid__item--footer">
-            <h4>Visual Wall Decorations</h4>
-          </div>
-        </div>
       </div>
     )
   }
