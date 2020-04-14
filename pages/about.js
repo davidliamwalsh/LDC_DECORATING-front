881a54efaa07f2e08caae07ab725278c431df9c5
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-04-14 04:25:11 +0100

    about changes

--- pages/about.js
+++ pages/about.js
@@ -9,8 +9,8 @@ class About extends Component {
   render () {
     return <Layout>
       <div className="o-head--intro">
-        <h4 className="o-head--intro__title">Established in 1984, LDC Decorating Contractors has grown into a leading regional contractor directly employing a skilled workforce of over fifty time served tradesmen operating primarily across the North of England.</h4>
-        <h5 className="o-head--intro__inset">The team at LDC Decorating are fully qualified in all aspects of the decorating trade and we offer all of our clients a first class service, no matter how small or large the job. We can provide advice and ideas to make sure that the job doesn't just look good - it also lasts. We pride ourselves on our reputation for quality and reliability.
+        <h4 className="o-head--intro__title">The company was founded 15 years ago and has grown from strength to strength, our philosophy is to provide the customer with a professional experience from start to finish. and we pride ourselves on being friendly, reliable, tidy, responsible priced, and high finish of work.</h4>
+        <h5 className="o-head--intro__inset">We are fully insured to 2 million, and can also provide a full risk assessment and method statement for the bigger jobs. We have a large team of fully vetted and experienced decorators on board that will go the extra mile. We can provide advice and ideas to make sure that the job doesn't just look good - it also lasts.
         </h5>
       </div>
       <div className="o-section-head">
